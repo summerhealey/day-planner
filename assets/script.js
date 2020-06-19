c365338cf33f7331d286c691b1076a8d63679425
Summer Healey <summerleigh.healey@gmail.com>
2020-06-19 19:20:47 -0400

    Corrected responsiveness for smaller screen, but xtra-small screen still unresponsive. Fixed local storage so that save button actually saved the data and continued to display data in appropriate text area.

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,9 @@
 $(document).ready(function () {
   //DISPLAY DATE
   const $currentDay = $("#currentDay");
-  const $currentTime = $("#currentTime");
   const $container = $(".container");
-  let now = moment().format("dddd, MMMM Do");
-  $currentDay.text(now);
+  let nowDay = moment().format("dddd, MMMM Do");
+  $currentDay.text(nowDay);
 
   //SAVE BUTTON
   $(".saveBtn").on("click", function () {
@@ -15,20 +14,16 @@ $(document).ready(function () {
     console.log(localStorage);
   });
 
-  $("#hour-8 .description").val(localStorage.getItem("hour-1"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-2"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-3"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-4"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-5"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-6"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-7"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-8"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-10"));
-
-  //TIMEBLOCKS
-  /*Description: Hour, text area, and save button are presented by the hour 
-for all business hours between 9AM and 5PM.*/
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
   // PAST, PRESENT, FUTURE
   function compareHour() {
