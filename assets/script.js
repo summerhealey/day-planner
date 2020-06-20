a1cd10d527f2bedf81b7f202a04cf6027e63fbcb
Summer Healey <summerleigh.healey@gmail.com>
2020-06-19 21:37:09 -0400

    Added comments to explain HTML and JavaScript code.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
 //Application Code
 $(document).ready(function () {
   //DISPLAY DATE
+  /*Uses moment.js to display current date*/
   const $currentDay = $("#currentDay");
   const $container = $(".container");
   let nowDay = moment().format("dddd, MMMM Do");
   $currentDay.text(nowDay);
 
   //SAVE BUTTON
+  /*Once button is clicked, storage is set by collecting the value of the text area 
+  and connecting it to it's corresponding hour*/
   $(".saveBtn").on("click", function () {
     var activity = $(this).siblings(".description").val();
     var hour = $(this).parent().attr("id");
     localStorage.setItem(hour, activity);
     console.log(localStorage);
   });
+
+  //LOCAL STORAGE 
+  /*Activity is displayed in hour corresponding text area once saved to Local Storage*/
 
   $("#hour-8 .description").val(localStorage.getItem("hour-8"));
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
@@ -26,6 +32,8 @@ $(document).ready(function () {
   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
   // PAST, PRESENT, FUTURE
+  /*Function compareHour compares current hour from moment.js to the hour of the time block to determine whether the  
+  hour has passed, is current, or is in the future*/
   function compareHour() {
     var currentHour = moment().hour();
 
@@ -46,5 +54,5 @@ $(document).ready(function () {
     });
   }
 compareHour();
-  //CLEAR BUTTON
+  
 });
